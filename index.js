28e0beb8c328a0e29f911bc6082712b53b38ef23
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-03-05 11:39:32 +0300

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { CONNECTIONS } from "https://f3be20b4-2c39-4c6a-b6f5-76ded0d8e655.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/index.js";
+import { FUCKCONNECTION } from "https://f3be20b4-2c39-4c6a-b6f5-76ded0d8e655.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/index.js";
 
 export const CONNECTIONS=()=>{
 
-    CONNECTIONS();
+    FUCKCONNECTION();
 
 };
