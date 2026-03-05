b54ecd1f74122b45e8b84744ad0d6b1abc946b4f
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-03-05 11:33:32 +0300

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-export const CONNECTION=()=>{
+import { CONNECTIONS } from "https://f3be20b4-2c39-4c6a-b6f5-76ded0d8e655.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/index.js";
+
+export const CONNECTIONS=()=>{
+
+    CONNECTIONS();
 
 };
