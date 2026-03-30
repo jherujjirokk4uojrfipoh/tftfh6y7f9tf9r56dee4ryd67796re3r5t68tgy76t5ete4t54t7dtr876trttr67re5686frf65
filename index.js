ad4747e5383d05b80de3bbddf245046ae009431e
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-03-30 18:36:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import { FUCKCONNECTION } from "https://f3be20b4-2c39-4c6a-b6f5-76ded0d8e655.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/index.js";
 import { CONNECTION } from "https://f3be20b4-2c39-4c6a-b6f5-76ded0d8e655.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655/Index.js";
 
-export const CONNECTIONS=()=>{
-
-    if ( localStorage.getItem("Name")||localStorage.getItem("Name") === "Erand") {
-
-        CONNECTION();
-        
-    } else {
-
-        FUCKCONNECTION();
-        
-    };
-
+export const CONNECTIONS = () => {
+  const name = localStorage.getItem("Name");
+  if (name === "Erand") {
+    CONNECTION();
+  } else {
+    FUCKCONNECTION();
+  }
 };
