17c55d24a620e1d413f47bdc10d2f6e8cbde9481
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-03-30 17:29:03 +0300

    EXIDION
    The Connection Point to Handle Two Frame Works,One Being Built and the Other Already In Production

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import { FUCKCONNECTION } from "https://f3be20b4-2c39-4c6a-b6f5-76ded0d8e655.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/index.js";
+import { CONNECTION } from "https://f3be20b4-2c39-4c6a-b6f5-76ded0d8e655.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655/Index.js";
 
 export const CONNECTIONS=()=>{
 
-    FUCKCONNECTION();
+    if ( localStorage.getItem("Name")||localStorage.getItem("Name") === "Erand") {
+
+        CONNECTION();
+        
+    } else {
+
+        FUCKCONNECTION();
+        
+    };
 
 };
