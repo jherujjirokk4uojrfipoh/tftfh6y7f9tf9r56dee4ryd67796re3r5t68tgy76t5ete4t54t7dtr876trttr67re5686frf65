b331196d39d87a739ffa38dfd890268a92c92daf
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-03-30 18:44:53 +0300

    Correction of Connection point

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import { FUCKCONNECTION } from "https://f3be20b4-2c39-4c6a-b6f5-76ded0d8e655.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/index.js";
-import { CONNECTION } from "https://f3be20b4-2c39-4c6a-b6f5-76ded0d8e655.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655/Index.js";
 
-export const CONNECTIONS = () => {
-  const name = localStorage.getItem("Name");
-  if (name === "Erand") {
-    CONNECTION();
-  } else {
+export const CONNECTIONS=()=>{
+
     FUCKCONNECTION();
-  }
+
 };
